3cfed7ff0f3f2d5a658643d04ae5439149781bcb
Yun Trinh <yktrinh@gmail.com>
2016-07-31 12:49:12 -0700

    had issues with earlier commit. fixes bugs in code for not using the correct dB values, spelling of to_close_vals to too_close_vals, and removed unnecessary update on dB array during cosolidation

--- src/matlab/spectral_analysis/consolidate_peaks.m
+++ src/matlab/spectral_analysis/consolidate_peaks.m
@@ -1,21 +1,34 @@
 function [peak] = consolidate_peaks(peak, smoothed_dB, min_bin_gap)
     
+    % find the indices where peaks are too close
     peak_dist = diff(peak);
     too_close_idx = find(peak_dist < min_bin_gap);
-
+    
+    % if there are peaks that are too close, consolidate them by merging
+    % the largest dB valued peaks first then repeat till there are no more
+    % peaks that are close
     while(~isempty(too_close_idx))
         fprintf('Consolidating peaks\n');
-        to_close_vals = smoothed_dB(too_close_idx);
-        [~, maxidx] = max(to_close_vals);
+        % get the dB values of all the too close peaks
+        too_close_vals = smoothed_dB(peak(too_close_idx));
+        
+        % find the index to the peak with the max dB value
+        [~, maxidx] = max(too_close_vals);
+        % get the index of the peak diffs that corresponds to the max dB 
+        % value from the too close indices
         max_freq_idx = too_close_idx(maxidx);
         
+        % set the max_offset by finding the smaller dB value between the
+        % too close peaks (want to delete the smaller dB value)
         [~, max_offset] = ...
                min(smoothed_dB(peak([max_freq_idx; max_freq_idx+1])));
+        
+        % set the index to delete from the peak array
         delete_idx = max_freq_idx - 1 + max_offset;
-           
         peak(delete_idx) = [];
-        smoothed_dB(delete_idx) = [];
-
+        
+        % recalculate diffs and find if there are more peaks that are too
+        % close
         peak_dist = diff(peak);
         too_close_idx = find(peak_dist < 2);
     end
